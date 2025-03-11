7fbe94807b5afc9ad78cfe7a2a36ca00dc06e561
BinarySemaphore <buetowbp@gmail.com>
2025-03-10 23:17:10 -0500

    Majority of the Win32 App Select file or pull from Ylands. Name and type output. Execute ExtractorV2 with args and print output.

--- extractor2/src/winapp/winapp.cpp
+++ extractor2/src/winapp/winapp.cpp
@@ -1,10 +1,13 @@
 #include "winapp.hpp"
 
-#include <tchar.h>
+#include <filesystem>
+#include <shobjidl.h>
 
 HINSTANCE hinst;
-int win_width = 500;
-int win_height = 500;
+int width = 800;
+int height = 500;
+int min_width = 570;
+int min_height = 500;
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hinstance,
@@ -24,7 +27,7 @@ int WINAPI WinMain(
 	wcex.hInstance = hinst;
 	wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW);
 	wcex.lpszMenuName = NULL;
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
@@ -41,15 +44,11 @@ int WINAPI WinMain(
 
 	HWND hwnd = CreateWindowEx(
 		WS_EX_OVERLAPPEDWINDOW,
-		szWindowClass,
-		"Ylands Extractor (GUI)",
+		szWindowClass, "Ylands Extractor (GUI)",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
-		win_width, win_height,
-		NULL,
-		NULL,
-		hinst,
-		NULL
+		width, height,
+		NULL, NULL, hinst, NULL
 	);
 
 	if (!hwnd) {
@@ -74,49 +73,281 @@ int WINAPI WinMain(
 	return (int)msg.wParam;
 }
 
-#define ID_BUTTON_ONE 1001
-#define ID_LOGBOX_ONE 2001
-HWND hlog = NULL;
+#define ID_BUTTON_LOAD 1001
+#define ID_BUTTON_CLEAR 1002
+#define ID_BUTTON_EXECUTE 1003
+#define ID_NAMEBOX 2001
+#define ID_TYPEBOX 2002
+#define ID_LOGBOX 2003
+HWND hgrp_input;
+HWND hgrp_output;
+HWND hgrp_run;
+HWND hclear;
+HWND hinput;
+HWND hname;
+HWND htype;
+HWND hlog;
+char output_filename[250] = "";
+char output_type[100] = "";
+char input_default[] = "Ylands Direct Extraction";
+char input_filepath[250] = "";
+std::vector<std::string> types = {"JSON", "OBJ"};
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
-	PAINTSTRUCT ps;
-	HDC hdc;
+	int rwidth, rheight, pwidth, pheight, rx, ry;
+	int margin = 10;
+	int padding = 6;
+	int half_padding = padding / 2;
+	RECT r;
 
-	switch (message)
-	{
+	switch (message) {
+	case WM_GETMINMAXINFO: {
+		MINMAXINFO* minMaxInfo = (MINMAXINFO*)lparam;
+		minMaxInfo->ptMinTrackSize.x = min_width;
+		minMaxInfo->ptMinTrackSize.y = min_height;
+		return 0;
+	}
+	case WM_CTLCOLORSTATIC: {
+		HDC hdc = (HDC)wparam;
+		if ((HWND)lparam == hlog) {
+			SetBkColor(hdc, RGB(200, 200, 200));
+		}
+		return 0;
+	}
 	case WM_CREATE:
+		// Actual dims
+		GetClientRect(hwnd, &r);
+		width = r.right - r.left;
+		height = r.bottom - r.top;
+
+		// Relative dims
+		rwidth = width - (2 * margin);
+		rheight = height - (2 * margin);
+
+		// Top level groupings
+		hgrp_input = CreateWindowEx(
+			0,
+			"BUTTON", "Input",
+			WS_CHILD | WS_VISIBLE | BS_GROUPBOX | WS_EX_ACCEPTFILES,
+			margin, margin,
+			rwidth / 3 - half_padding, rheight / 3 - half_padding,
+			hwnd, (HMENU)NULL, hinst, NULL
+		);
+		hgrp_output = CreateWindowEx(
+			0,
+			"BUTTON", "Output Options",
+			WS_CHILD | WS_VISIBLE | BS_GROUPBOX,
+			margin, margin + rheight / 3 + half_padding,
+			rwidth / 3 - half_padding, rheight * 2 / 3 - half_padding,
+			hwnd, (HMENU)NULL, hinst, NULL
+		);
+		hgrp_run = CreateWindowEx(
+			0,
+			"BUTTON", "Run",
+			WS_CHILD | WS_VISIBLE | BS_GROUPBOX,
+			margin + rwidth / 3 + half_padding, margin,
+			rwidth * 2 / 3 - half_padding, rheight,
+			hwnd, (HMENU)NULL, hinst, NULL
+		);
+
+		// Input Group
+		rx = margin;
+		ry = margin + 20;
 		CreateWindowEx(
-			0, "BUTTON", "Run Command",
+			0,
+			"BUTTON", "Select Existing",
 			WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_DEFPUSHBUTTON,
-			5, 5, 150, 30,
-			hwnd, (HMENU)ID_BUTTON_ONE, hinst, NULL
+			rx + padding, ry + padding,
+			115, 30,
+			hwnd, (HMENU)ID_BUTTON_LOAD, hinst, NULL
+		);
+		hclear = CreateWindowEx(
+			0,
+			"BUTTON", "Clear Input",
+			WS_CHILD | WS_TABSTOP | BS_DEFPUSHBUTTON,
+			rx + padding + (padding + 115), ry + padding,
+			90, 30,
+			hwnd, (HMENU)ID_BUTTON_CLEAR, hinst, NULL
+		);
+		CreateWindowEx(
+			0,
+			"STATIC", "Using:",
+			WS_CHILD | WS_VISIBLE,
+			rx + padding, ry + padding + (padding + 30),
+			40, 18,
+			hwnd, (HMENU)NULL, hinst, NULL
+		);
+		hinput = CreateWindowEx(
+			0,
+			"STATIC", input_default,
+			WS_CHILD | WS_VISIBLE | WS_BORDER | BS_DEFPUSHBUTTON,
+			rx + padding, ry + padding + (2 * padding + 30 + 18),
+			rwidth / 3 - half_padding - 2 * padding, 20,
+			hwnd, (HMENU)NULL, hinst, NULL
+		);
+
+		// Output Options Group
+		rx = margin;
+		ry = margin + rheight / 3 + half_padding + 20;
+		CreateWindowEx(
+			0,
+			"STATIC", "Name (optional):",
+			WS_CHILD | WS_VISIBLE,
+			rx + padding, ry + padding,
+			110, 18,
+			hwnd, (HMENU)NULL, hinst, NULL
+		);
+		hname = CreateWindowEx(
+			0,
+			"EDIT", "",
+			WS_CHILD | WS_VISIBLE | WS_BORDER,
+			rx + padding, ry + padding + (padding + 18),
+			rwidth / 3 - half_padding - 2 * padding, 20,
+			hwnd, (HMENU)ID_NAMEBOX, hinst, NULL
+		);
+		CreateWindowEx(
+			0,
+			"STATIC", "Type:",
+			WS_CHILD | WS_VISIBLE,
+			rx + padding, ry + padding + (2 * padding + 18 + 20),
+			40, 18,
+			hwnd, (HMENU)NULL, hinst, NULL
+		);
+		htype = CreateWindowEx(
+			0,
+			"COMBOBOX", "TYPE",
+			WS_CHILD | WS_VISIBLE | CBS_DROPDOWN | CBS_HASSTRINGS,
+			rx + padding + (padding + 40), ry + padding + (2 * padding + 18 + 20),
+			80, 200,
+			hwnd, (HMENU)ID_TYPEBOX, hinst, NULL
+		);
+		for (const std::string& type : types) {
+			SendMessage(htype, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(type.c_str()));
+		}
+		SendMessage(htype, CB_SETCURSEL, 0, 0);
+
+		// Run Group
+		rx = margin + rwidth / 3 + half_padding;
+		ry = margin + 20,
+		pwidth = rwidth * 2 / 3 - half_padding - (2 * padding);
+		pheight = rheight - (2 * padding + 20);
+		CreateWindowEx(
+			0,
+			"BUTTON", "Save Output",
+			WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_DEFPUSHBUTTON,
+			rx + padding, ry + padding,
+			100, 30,
+			hwnd, (HMENU)ID_BUTTON_EXECUTE, hinst, NULL
 		);
 		hlog = CreateWindowEx(
-			0, "EDIT", NULL,
-			WS_CHILD | WS_VISIBLE | ES_MULTILINE | WS_VSCROLL |
-			ES_AUTOVSCROLL | ES_READONLY,
-			5, 35, win_width - 5, win_height - 35,
-			hwnd, (HMENU)ID_LOGBOX_ONE, hinst, NULL
+			0,
+			"EDIT", "Status:",
+			WS_CHILD | WS_VISIBLE | WS_BORDER |
+			ES_MULTILINE | WS_VSCROLL | ES_AUTOVSCROLL | ES_READONLY,
+			rx + padding, ry + padding + (padding + 30),
+			pwidth, pheight - (padding + 30),
+			hwnd, (HMENU)ID_LOGBOX, hinst, NULL
 		);
 		break;
 	case WM_SIZE:
-		win_width = LOWORD(lparam);
-		win_height = HIWORD(lparam);
-		SetWindowPos(hlog, NULL, 0, 0, win_width - 5, win_height - 35, SWP_NOMOVE | SWP_NOZORDER);
+		// Using r* as delta
+		rwidth = LOWORD(lparam) - width;
+		rheight = HIWORD(lparam) - height;
+
+		// Actual dims
+		width = LOWORD(lparam);
+		height = HIWORD(lparam);
+
+		GetWindowRect(hgrp_run, &r);
+		pwidth = r.right - r.left;
+		pheight = r.bottom - r.top;
+		SetWindowPos(hgrp_run, NULL, 0, 0, pwidth + rwidth, pheight + rheight, SWP_NOMOVE | SWP_NOZORDER);
+
+		GetWindowRect(hlog, &r);
+		pwidth = r.right - r.left;
+		pheight = r.bottom - r.top;
+		SetWindowPos(hlog, NULL, 0, 0, pwidth + rwidth, pheight + rheight, SWP_NOMOVE | SWP_NOZORDER);
 		break;
 	case WM_COMMAND:
-		if (LOWORD(wparam) == ID_BUTTON_ONE) {
-			if (!RunCommandAndCaptureOutput("cmd.exe /c ExtractorV2.exe")) {
+		if (LOWORD(wparam) == ID_BUTTON_LOAD) {
+			HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+			if (SUCCEEDED(hr)) {
+				IFileOpenDialog *pFileOpen;
+				// Create the FileOpenDialog object.
+				hr = CoCreateInstance(
+					CLSID_FileOpenDialog, NULL, CLSCTX_ALL, 
+					IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen)
+				);
+				if (SUCCEEDED(hr)) {
+					// Show open dialog box at PWD
+					char c_pwd[250];
+					wchar_t pwd[250];
+					IShellItem *pCurFolder = NULL; 
+					GetCurrentDirectory(250, c_pwd);
+					mbstowcs_s(NULL, pwd, c_pwd, std::strlen(c_pwd) + 1);
+					hr = SHCreateItemFromParsingName((PCWSTR)pwd, NULL, IID_PPV_ARGS(&pCurFolder));
+					if (SUCCEEDED(hr))
+					{
+						pFileOpen->SetDefaultFolder(pCurFolder);
+						pCurFolder->Release();
+					}
+					COMDLG_FILTERSPEC filter[1] = {{L"JSON Files", L"*.json"}};
+					pFileOpen->SetFileTypes(1, filter);
+					hr = pFileOpen->Show(NULL);
+		
+					// Get the file name from the dialog box.
+					if (SUCCEEDED(hr)) {
+						IShellItem *pItem;
+						hr = pFileOpen->GetResult(&pItem);
+						if (SUCCEEDED(hr)) {
+							PWSTR pszFilePath;
+							hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+							if (SUCCEEDED(hr)) {
+								wcstombs_s(NULL, input_filepath, pszFilePath, 250);
+								CoTaskMemFree(pszFilePath);
+								std::filesystem::path fp = input_filepath;
+								char fp_name[250];
+								strcpy_s(fp_name, fp.filename().string().c_str());
+								SetWindowText(hinput, fp_name);
+								ShowWindow(hclear, SW_SHOW);
+							}
+							pItem->Release();
+						}
+					}
+					pFileOpen->Release();
+				}
+				CoUninitialize();
+			}
+		} else if (LOWORD(wparam) == ID_BUTTON_EXECUTE) {
+			char c_cmd[250];
+			std::string cmd = "cmd.exe /c ExtractorV2.exe";
+			if (input_filepath[0] != '\0') {
+				cmd += " -i " + std::string(input_filepath);
+			}
+			if (output_filename[0] != '\0') {
+				cmd += " -o \"" + std::string(output_filename) + "\""; 
+			}
+			if (output_type[0] != '\0') {
+				cmd += " -t " + std::string(output_type);
+			}
+			strcpy_s(c_cmd, cmd.c_str());
+			if (!RunCommandAndCaptureOutput(c_cmd)) {
 				int log_length = GetWindowTextLength(hlog);
 				SendMessage(hlog, EM_SETSEL, (WPARAM)log_length, (LPARAM)log_length);
 				SendMessage(hlog, EM_REPLACESEL, FALSE, (LPARAM)"Failed to execute command.");
 			}
+		} else if (LOWORD(wparam) == ID_BUTTON_CLEAR) {
+			ShowWindow(hclear, SW_HIDE);
+			input_filepath[0] = '\0';
+			SetWindowText(hinput, input_default);
+		} else if (LOWORD(wparam) == ID_NAMEBOX) {
+			GetWindowText(hname, output_filename, 250);
+		} else if (LOWORD(wparam) == ID_TYPEBOX) {
+			int selectedIndex = SendMessage(htype, CB_GETCURSEL, 0, 0);
+            if (selectedIndex != CB_ERR) {
+                SendMessage(htype, CB_GETLBTEXT, selectedIndex, reinterpret_cast<LPARAM>(output_type));
+            }
 		}
-		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hwnd, &ps);
-		SetBkMode(hdc, TRANSPARENT);
-		EndPaint(hwnd, &ps);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -146,6 +377,10 @@ bool RunCommandAndCaptureOutput(char* command) {
     si.hStdInput = NULL;
     PROCESS_INFORMATION pi;
 
+	WriteToLogBox("\r\n______________________________\r\n");
+	WriteToLogBox(command);
+	WriteToLogBox("\r\n==============================\r\n");
+
     if (!CreateProcess(
         NULL, command, NULL, NULL, TRUE,
         CREATE_NO_WINDOW, NULL, NULL,
@@ -162,9 +397,7 @@ bool RunCommandAndCaptureOutput(char* command) {
     DWORD bytesRead;
     while (ReadFile(hRead, buffer, 1024 - 1, &bytesRead, NULL) && bytesRead > 0) {
         buffer[bytesRead] = '\0';
-		log_length = GetWindowTextLength(hlog);
-		SendMessage(hlog, EM_SETSEL, (WPARAM)log_length, (LPARAM)log_length);
-		SendMessage(hlog, EM_REPLACESEL, FALSE, (LPARAM)buffer);
+		WriteToLogBox(buffer);
     }
 
     CloseHandle(hRead);
@@ -172,4 +405,10 @@ bool RunCommandAndCaptureOutput(char* command) {
     CloseHandle(pi.hThread);
 
 	return true;
+}
+
+void WriteToLogBox(const TCHAR* text) {
+	int log_length = GetWindowTextLength(hlog);
+	SendMessage(hlog, EM_SETSEL, (WPARAM)log_length, (LPARAM)log_length);
+	SendMessage(hlog, EM_REPLACESEL, FALSE, (LPARAM)text);
 }
--- extractor2/src/winapp/winapp.hpp
+++ extractor2/src/winapp/winapp.hpp
@@ -1,6 +1,8 @@
 #include <windows.h>
 #include <string>
+#include <tchar.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 bool RunCommandAndCaptureOutput(char* command);
+void WriteToLogBox(const TCHAR* text);
